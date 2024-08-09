3b15b3d94ecaf34dcf22c1ba1705b7eb28f59b69
ParasGurkhude <parasgurkhude@gmail.com>
2024-08-09 13:50:23 +0530

    Added a new project of flipkart clone

---
+++ index.js
@@ -0,0 +1,18 @@
+let header = document.querySelector("header");
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+  header.classList.toggle("shadow", window.scrollY > 0);
+});
+
+menu.onclick = () => {
+  navbar.classList.toggle("active");
+};
+window.onscroll = () => {
+  navbar.classList.remove("active");
+};
+
+document.getElementById('resume-button-1').addEventListener("click", () => {
+  window.open("https://drive.google.com/file/d/1Jptdik6ICzxY8sZym79hl_I_kDH2IBsY/view?usp=sharing", "_blank");
+});
